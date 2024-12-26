30f3f25017506c9e699f910067fea9e2f2e2dac2
Fernando <palhares.f@gmail.com>
2024-12-26 10:38:34 -0300

    api file set to receive GET form, calling most appropriate method handlers; convertHandler handling many of the required cases

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -2,19 +2,29 @@ function ConvertHandler() {
   
   this.getNum = function(input) {
     let result;
-    
+    result = parseFloat(input[1]);
     return result;
   };
   
   this.getUnit = function(input) {
     let result;
-    
+    result = input[2].trim();
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
     let result;
-    
+    switch (initUnit) {
+      case 'gal':
+        result = 'L';
+        break;
+      case 'lbs':
+        result = 'kg';
+        break;
+      case 'mi':
+        result = 'km';
+        break;
+    }
     return result;
   };
 
@@ -29,7 +39,17 @@ function ConvertHandler() {
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
+    switch (initUnit) {
+      case 'gal':
+        result = initNum * galToL;
+        break;
+      case 'lbs':
+        result = initNum * lbsToKg;
+        break;
+      case 'mi':
+        result = initNum * miToKm;
+        break;
+    }    
     return result;
   };
   
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,32 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
 
+  app.get("/api/convert", function (req, res) {
+    const match = req.query.input.match(/^(\d+(?:\.\d+)?)(.*)$/);
+    if (match) {
+      const initNum = convertHandler.getNum(match);
+      const initUnit = convertHandler.getUnit(match);
+      const returnNum = convertHandler.convert(initNum, initUnit);
+      const returnUnit = convertHandler.getReturnUnit(initUnit);
+      const string = convertHandler.getString(
+        initNum,
+        initUnit,
+        returnNum,
+        returnUnit
+      );
+
+      res.json({
+        initNum,
+        initUnit,
+        returnNum,
+        returnUnit,
+        string,
+      });
+    }
+  });
 };
